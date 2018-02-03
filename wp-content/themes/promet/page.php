5d1bd57f91505bd938db1507102f4a4d055a4a4e
Roman Kowalski <kowal2499@gmail.com>
2018-02-03 18:18:16 +0100

    responsywny header

--- wp-content/themes/promet/page.php
+++ wp-content/themes/promet/page.php
@@ -2,47 +2,55 @@
 
 	<header id="header">
 		<div class="container">
-			<div class="tile-logo">
-				<img src="http://promet.local/wp-content/uploads/2018/02/logo.png" alt="PROMET logo">
-			</div>
 
-			<div class="tile-info">
-				<div class="icon">
-					<i class="far fa-clock fa-2x"></i>
+			<div class="infoBar">
+				<div class="sideLeft">
+					<div class="tile">
+						<img src="http://promet.local/wp-content/uploads/2018/02/logo.png" alt="PROMET logo">	
+					</div>
 				</div>
-				<div class="desc">
-					<ul>
-						<li class="rowTop"><strong>Pon-Pt</strong></li>
-						<li class="rowBottom">8:00-16:00</li>
-					</ul>
+
+				<div class="sideRight">
+
+					<div class="tile">
+						<div class="icon">
+							<i class="fas fa-phone-volume fa-2x"></i>
+						</div>
+						<div class="desc">
+							<ul>
+								<li class="rowTop"><strong>2-888-123-4567</strong></li>
+								<li class="rowBottom">info@buildpress.com</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="tile">
+						<div class="icon">
+							<i class="fas fa-globe fa-2x"></i>
+						</div>
+						<div class="desc">
+							<ul>
+								<li class="rowTop"><strong>78-200 Białogard</strong></li>
+								<li class="rowBottom">ul. Chocimska 5</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="tile">
+						<div class="icon">
+							<i class="fas fa-clock fa-2x"></i>
+						</div>
+						<div class="desc">
+							<ul>
+								<li class="rowTop"><strong>Pon-Pt</strong></li>
+								<li class="rowBottom">8:00-16:00</li>
+							</ul>
+						</div>
+					</div>
+
+
 				</div>
 			</div>
-
-			<div class="tile-info">
-				<div class="icon">
-					<i class="fas fa-globe fa-2x"></i>
-				</div>
-				<div class="desc">
-					<ul>
-						<li class="rowTop"><strong>78-200 Białogard</strong></li>
-						<li class="rowBottom">ul. Chocimska 5</li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="tile-info">
-				<div class="icon">
-					<i class="fas fa-phone-volume fa-2x"></i>
-				</div>
-				<div class="desc">
-					<ul>
-						<li class="rowTop"><strong>1-888-123-4567</strong></li>
-						<li class="rowBottom">info@buildpress.com</li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="clearfix"></div>
 			
 			<div class="menuBar"></div>
 
