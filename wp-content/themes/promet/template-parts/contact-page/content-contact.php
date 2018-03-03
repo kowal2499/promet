9815043fc8ef903240f82bf0d16bcd77de8d503f
Roman Kowalski <kowal2499@gmail.com>
2018-03-03 22:17:11 +0100

    początek

---
+++ wp-content/themes/promet/template-parts/contact-page/content-contact.php
@@ -0,0 +1,43 @@
+
+<div class="col-md-6">
+    <h3>
+        Kontakt z firmą
+    </h3>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid atque eum earum voluptate voluptas quisquam, magni provident quasi quos fuga sunt error modi sint, soluta vitae aut numquam mollitia voluptates maxime? Distinctio quam veritatis rem animi nemo mollitia commodi iure?</p>
+
+    <form action="">
+        <div class="row">
+            <div class="form-group col-sm-6">
+                <label for="name">Imię & Nazwisko</label>
+                <input type="text" class="form-control required name" placeholder="Podaj imię i nazwisko">
+            </div>
+
+            <div class="form-group col-sm-6">
+                <label for="email">Email</label>
+                <input type="text" class="form-control required email" placeholder="Podaj email">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-sm-12">
+                <label for="subject">Temat wiadomości</label>
+                <input type="text" class="form-control required subject" placeholder="Temat">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-sm-12">
+                <label for="body">Treść wiadomości</label>
+                <textarea type="text" rows="5" class="form-control required body" placeholder="Treść"></textarea>
+            </div>
+        </div>
+
+    </form>
+</div>
+
+
+
+<div class="col-md-6">
+
+</div>
