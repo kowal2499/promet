c360bb18290bc5b09de000c9815d8252f2ac0932
Roman Kowalski <kowal2499@gmail.com>
2018-03-03 21:44:26 +0100

    dodane archiwum produktów

--- wp-content/themes/promet/template-parts/archive/content-products.php
+++ wp-content/themes/promet/template-parts/archive/content-products.php
@@ -1,3 +1,44 @@
+<?php
+    $settings = Settings::getInstance();
+    $productsData = Products::getInstance()->getProducts();
+?>
+
 <section id="archive-products">
-    tutaj jestem
+    <?php
+        foreach ($productsData as $id => $category):
+    ?>
+        <article>
+            <div class="container">
+                <div class="menu">
+                    <ul>
+                        <li class="active"><div class="valign"><div class="valingContent"><?php echo $category['name']; ?></div></div></li>
+                    </ul>
+                </div><!-- menu -->
+
+                <div class="showcase">
+                <?php
+                    foreach ($category['products'] as $product):
+                        echo '<div class="specimen" data-category="'. $cat_id .'">';
+                        echo '<a href="'. $product['link'] .'">';
+                        echo '<div class="wrapper"><img src="'. $product['photoMain'] .'">';
+                        echo '<div class="courtain">';
+                        echo '<div class="valign"><div class="valingContent">';
+                        echo $product['excerpt'];
+                        echo '</div></div>';
+                        echo '</div>';
+                        echo '</div>';
+                        echo '<hr>';
+                        echo '<h3>'. $product[title] .'</h3>';
+                        echo '</a>';
+                        echo '</div>';
+                    endforeach;
+                ?>
+                </div><!-- showcase -->
+            </div>
+        </article>   
+
+    <?php
+        endforeach;
+    ?>
+
 </section>
